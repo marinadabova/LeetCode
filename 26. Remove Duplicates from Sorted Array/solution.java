071c42990f908623e4cf1b5844a182c829a286ae
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2024-11-07 12:12:30 +0200

    Update solution.java

--- 26. Remove Duplicates from Sorted Array/solution.java
+++ 26. Remove Duplicates from Sorted Array/solution.java
@@ -10,3 +10,21 @@ class Solution {
         return k;
     }
 }
+//two pointers- l for equals, r for non-equals
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=nums.length;
+        int l=0;
+        int r=1;
+
+        while(r<n){
+            if(nums[l]==nums[r]){
+                r++;
+            }else if(nums[l]!=nums[r]){
+                l++;
+                nums[l]=nums[r];
+            }
+        }
+        return l+1;
+    }
+}
