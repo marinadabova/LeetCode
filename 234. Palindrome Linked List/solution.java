c4f99082152336f4dbccdba661417b1fad43f3fd
Marina Dabova <107205614+marinadabova@users.noreply.github.com>
2024-11-14 19:52:51 +0200

    LinkedList/twoPtr

---
+++ 234. Palindrome Linked List/solution.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode start = head;
+        ListNode end = reverse(slow);
+        while (end != null) {
+            if (start.val == end.val) {
+                start = start.next;
+                end = end.next;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
